cfb43cb0341c51ba86bf334ee3042059fc3e2fef
Stanley Huang <infamous_godhand@yahoo.com>
2021-07-01 08:19:40 +1000

    configure custom hook by passing argument

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
-export const useCounter = () => {
+export const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
